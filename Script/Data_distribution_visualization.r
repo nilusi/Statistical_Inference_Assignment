a2541d635e62184f439f210833a4b3f27fded7db
nilusija <nilusija@gmial.com>
2019-11-08 15:28:33 +0530

    Adding some figures

--- Script/Data_distribution_visualization.r
+++ Script/Data_distribution_visualization.r
@@ -3,13 +3,22 @@ library(readxl)
 #Load the response data
 election_data <- read_excel("D:/aapp/Statistical_Inference_Assignment/responses/Sri_Lanka_Presidential_Election _Responses _preprocessed.xlsx")
 
+#Gender distribution 
 interested_people <- subset(election_data, To_be_vote == "Yes")
 genderDistribution <- table(interested_people$gender)
-
 
 #Bar plot for gender distribution
 barplot(genderDistribution, main="Gender Distribution of voted people", col=c("red"),
         xlab="gender",ylab="Frequency")
+
+#Ethinic group distribution 
+interested_people <- subset(election_data, To_be_vote == "Yes")
+ethnicityDistribution <- table(interested_people$ethnic_group)
+
+
+#Bar plot for Ethinic group distribution
+barplot(ethnicityDistribution, main="Ethnic Group Distribution of voted people", col=c("blue"),
+        xlab="Ethnic Group",ylab="Voters count")
 
 
 
@@ -35,3 +44,12 @@ third_priority_candidates_result <- c(third_priority_candidates_result, c1)
 #Third priority candidate - Bar chart
 barplot(third_priority_candidates_result, main="Third priority candidate in voting",  names.arg = c("Anura","Gotabaya","Mahesh", "No one","Sajith","Others"),
         xlab="Candidates",ylab="Count")
+
+
+#Prior Presidents involvement in following the promises
+follow_promises <- table(election_data$promisses_followed)
+
+
+#Bar plot for Prior Presidents involvement in following the promises
+barplot(follow_promises, main="Prior Presidents involvement in following the promises", col=c("blue"),
+        xlab="Having_Involvement",ylab="Voters count")
